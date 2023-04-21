4be3da2633ac568f4906c0cd07326ce11f7ce27a
Akshay <131444186+danksenpai7@users.noreply.github.com>
2023-04-21 15:12:13 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -40,7 +40,7 @@ SONG_DOWNLOAD_DURATION = int(
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001822258046"))
 
 # A name for your Music bot.
-MUSIC_BOT_NAME = getenv("Miku")
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "Miku Music")
 
 # Your User ID.
 OWNER_ID = list(
@@ -65,10 +65,10 @@ GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 # Only  Links formats are  accepted for this Var value.
 SUPPORT_CHANNEL = getenv(
-    "SUPPORT_CHANNEL", None
+    "SUPPORT_CHANNEL", "https://t.me/AstorSupport"
 )  # Example:- https://t.me/AstorSupport 
 SUPPORT_GROUP = getenv(
-    "SUPPORT_GROUP", None
+    "SUPPORT_GROUP", "https://t.me/NakanoSupport"
 )  # Example:- https://t.me/NakanoSupport
 
 # Set it in True if you want to leave your assistant after a certain amount of time. [Set time via AUTO_LEAVE_ASSISTANT_TIME]
